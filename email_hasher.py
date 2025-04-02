10b8b7e745e4dd83061c159ae255a03022fd38b4
Horace C <181307191+hcheng25@users.noreply.github.com>
2025-04-02 11:38:31 -0700

    changed exit code

--- email_hasher.py
+++ email_hasher.py
@@ -36,8 +36,7 @@ def main():
         hash = hash_email(email)
         write_hash_to_file(hash_value=hash, filename='hash.email')
     else:
-        print('Error: argument is not a valid email address')
-        exit(1)
+        exit('Argument is not a valid email address')
     # 1. Check if an email address was provided as a command line argument
     # 2. If not, print an error message and exit with a non-zero status
     # 3. If yes, hash the email address
